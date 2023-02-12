3f70dfde164dc618f6b75e7064399ee96360fde4
MacdonaldRobinson <maocnly2001@gmail.com>
2023-02-12 15:18:25 -0700

    Moved quite commend to the lobby

--- Lobby/lobby.gd
+++ Lobby/lobby.gd
@@ -8,6 +8,9 @@ extends Node3D
 func _ready():
 	pass # Replace with function body.
 
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
